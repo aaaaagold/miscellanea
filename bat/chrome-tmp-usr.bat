d4987f23d3b7ac2be75ac0dcb3ca67c80a6c2241
temporary not available <m5uxi2dvmi16.0216025@nctu.edu.tw>
2018-01-22 11:52:05 +0800

    add incognito to chrome params
    chrome has built-in user management, but useless in incognito mode

--- bat/chrome-tmp-usr.bat
+++ bat/chrome-tmp-usr.bat
@@ -6,7 +6,7 @@ set dirPrefix=%TEMP%\User Data
 @echo %i%
 @set dir="%dirPrefix% %i%"
 @mkdir %dir% ^
- && ( echo>%dir%\"First Run" && "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=%dir% 
+ && ( echo>%dir%\"First Run" && "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --incognito --user-data-dir=%dir% 
  ping -n 2 localhost > nul 
  rmdir /s /q %dir% 
  exit )
