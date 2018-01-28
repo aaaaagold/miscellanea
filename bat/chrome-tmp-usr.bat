cf88c8d853e8231237040e4afcc8a268cdc34431
temporary not available <m5uxi2dvmi16.0216025@nctu.edu.tw>
2018-01-29 01:17:53 +0800

    add: check lockfile
    chrome restarting (after you change some configurations) becomes available (can normally remove created folder)

--- bat/chrome-tmp-usr.bat
+++ bat/chrome-tmp-usr.bat
@@ -1,13 +1,24 @@
 @set i=0
 set dirPrefix=%TEMP%\User Data
 
-:J1
+:checkUsableFolderName_UseIt
 @set /A i+=1
 @echo %i%
 @set dir="%dirPrefix% %i%"
 @mkdir %dir% ^
  && ( echo>%dir%\"First Run" && "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --incognito --user-data-dir=%dir% 
- ping -n 2 localhost > nul 
- rmdir /s /q %dir% 
- exit )
-@goto J1
+ goto mainProcessNotFound_check_lockfile 
+ exit /b 1 )
+@goto checkUsableFolderName_UseIt
+@goto END
+
+:mainProcessNotFound_check_lockfile
+@ping -n 2 localhost > nul 
+@if exist %dir%\lockfile (
+ goto mainProcessNotFound_check_lockfile 
+)
+@rmdir /s /q %dir% 
+@goto END
+
+:END
+exit /b 0
