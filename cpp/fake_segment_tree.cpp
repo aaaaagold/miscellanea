17ec1c9650b72b570bc25a0f1238efe23bba6038
OAO <10041465+aaaaagold@users.noreply.github.com>
2019-05-30 08:11:33 +0800

    Update fake_segment_tree.cpp

--- cpp/fake_segment_tree.cpp
+++ cpp/fake_segment_tree.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 template<class T>
-class segtree
+class segtree_sum
 {
     T **tree;
     size_t *sizes;
@@ -26,15 +26,15 @@ class segtree
         return rtv;
     }
 public:
-    segtree():tree(NULL),sizes(NULL),lv(0),sizen(0)
+    segtree_sum():tree(NULL),sizes(NULL),lv(0),sizen(0)
     {
         ;
     }
-    segtree(const T *arr,const size_t &n):tree(NULL),sizes(NULL),lv(0),sizen(0)
+    segtree_sum(const T *arr,const size_t &n):tree(NULL),sizes(NULL),lv(0),sizen(0)
     {
         init(arr,n);
     }
-    ~segtree()
+    ~segtree_sum()
     {
         if(tree)
         {
@@ -50,7 +50,7 @@ public:
     }
     void init(const T *arr,const size_t &n)
     {
-        this->~segtree();
+        this->~segtree_sum();
         if(n==0)
             return;
         sizen=n;
@@ -104,14 +104,14 @@ public:
     {
         if(target>=sizen) return;
         T delta=setas-tree[0][target];
-        for(int x=0;x<lv;++x)
+        for(unsigned x=0;x<lv;++x)
             tree[x][target>>x]+=delta;
     }
     void print() // debug
     {
-        for(int y=0;y<lv;++y)
+        for(unsigned y=0;y<lv;++y)
         {
-            for(int x=0,s=sizes[y];x<s;++x)
+            for(unsigned x=0,s=sizes[y];x<s;++x)
                 printf(" %d",tree[y][x]);
             printf("\n");
         }
@@ -132,6 +132,130 @@ public:
     }
 };
 
+template<class T>
+class segtree_max
+{
+	T **tree;
+	size_t *sizes;
+	size_t lv;
+	size_t sizen;
+public:
+	segtree_max():tree(NULL),sizes(NULL),lv(0),sizen(0)
+	{
+		;
+	}
+	segtree_max(const T *arr,const size_t &n):tree(NULL),sizes(NULL),lv(0),sizen(0)
+	{
+		init(arr,n);
+	}
+	~segtree_max()
+	{
+		if(tree)
+		{
+			for(size_t x=0,size=lv;x!=size;++x)
+			{
+				register T *tmp=tree[x];
+				if(tmp)
+					delete [] tmp;
+			}
+			delete [] tree;
+		}
+		if(sizes) delete [] sizes;
+	}
+	void init(const T *arr,const size_t &n)
+	{
+		this->~segtree_max();
+		if(n==0)
+			return;
+		sizen=n;
+
+		register size_t lvcnt=1;
+		for(size_t x=n;x!=1;x=(x>>1)+(x&1),++lvcnt)
+			;
+		lv=lvcnt;
+
+		if((sizes=new size_t[lvcnt]))
+			for(size_t m=n,x=0;x!=lvcnt;m=(m>>1)+(m&1),++x)
+				sizes[x]=m;
+		else return;
+
+		if((tree=new T*[lvcnt]))
+		{
+			for(register size_t m=n,x=0;x!=lvcnt;m=(m>>1)+(m&1),++x)
+				tree[x]=new T[m];
+
+			register T *pre,*now=tree[0];
+			register size_t s0,s1=n;
+				for(register size_t x=0;x!=s1;++x)
+					now[x]=arr[x];
+			for(size_t y(1);y!=lvcnt;++y)
+			{
+				s0=s1;
+				s1=sizes[y];
+				pre=now;
+				now=tree[y];
+				if((s0&1)==1)
+				{
+					--s1;
+					for(register size_t x=0;x!=s1;++x)
+					{
+						const T &tmp1=pre[x<<1];
+						const T &tmp2=pre[(x<<1)|1];
+						now[x]=tmp1<tmp2?tmp2:tmp1;
+					}
+					now[s1++]=pre[s0-1];
+				}
+				else
+				{
+					for(register size_t x=0;x!=s1;++x)
+					{
+						const T &tmp1=pre[x<<1];
+						const T &tmp2=pre[(x<<1)|1];
+						now[x]=tmp1<tmp2?tmp2:tmp1;
+					}
+				}
+			}
+		}
+		else return;
+	}
+	T rangeMax(size_t beg, size_t bak,size_t lv=0) const
+	{
+		if(beg>bak) return rangeMax(bak,beg,lv);
+		const T *arr=tree[lv];
+		if(bak<beg+3)
+		{
+			T rtv=arr[beg];
+			while(beg++!=bak) if(rtv<arr[beg]) rtv=arr[beg];
+			return rtv;
+		}
+		register size_t f1=(beg&1),f2=(bak&1);
+		if(f1|f2)
+		{
+			if(f1)
+			{
+				if(f2)
+				{
+					const T rtv=arr[beg],tmp=rangeMax((beg+1)>>1,bak>>1,lv+1);
+					return rtv<tmp?tmp:rtv;
+				}
+				else
+				{
+					const T L=arr[beg],R=arr[bak];
+					const T rtv=L<R?R:L,tmp=rangeMax((beg+1)>>1,(bak-1)>>1,lv+1);
+					return rtv<tmp?tmp:rtv;
+				}
+			}
+			else return rangeMax(beg>>1,bak>>1,lv+1);
+		}
+		else
+		{
+			const T rtv=arr[bak],tmp=rangeMax(beg>>1,(bak-1)>>1,lv+1);
+			return rtv<tmp?tmp:rtv;
+		}
+	}
+};
+
+
 int main(const int argc,const char *argv[])
 {
     int tmp,flag=0;
@@ -142,7 +266,7 @@ int main(const int argc,const char *argv[])
     for(int x=0;x<length;++x)
         arr[x]=1<<x;
 
-    segtree<int>s(arr,length);
+    segtree_sum<int>s(arr,length);
 
     printf("array length:");if(flag==0)printf("(default) "); printf("%d\n",length);
     printf("tree:");printf("\n");
